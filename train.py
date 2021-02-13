16286e92510aacfa3bce3bf0e44717b9c4721bdc
toehmler <treyoehmler@jimbo-vm.us-west1-b.c.radiant-arcade-297700.internal>
2021-02-13 22:06:24 +0000

    trained unet_v0.1 on 0-120

--- train.py
+++ train.py
@@ -10,11 +10,12 @@ import keras
 from metrics import dice_coef, dice_coef_loss
 import configparser
 import os
+import datetime
 
 
 # load config (used to get path to data and other model specifics)
 with open('config.json') as config_file:
-    config = json.load(config_file)
+    jconfig = json.load(config_file)
 
 ''' generate_data: generates normalized and shuffled data to train with
 @param: start the starting index of the patient to generate data from
@@ -26,7 +27,7 @@ def generate_data(start, end):
     y = []
     pbar = tqdm(total = (end - start))
     while current < end:
-        path = glob(config['root'] + '/*pat{}*'.format(current))[0]
+        path = glob(jconfig['root'] + '/*pat{}*'.format(current))[0]
         scans = load_scans(path)
         scans = norm_scans(scans)
         current_x = []
@@ -66,7 +67,7 @@ if __name__ == "__main__":
         model = unet()
         # save sumamry printout of model to directory
         og_stdout = sys.stdout
-        with open("{}/{}_summary.txt", "w") as f:
+        with open("{}/{}_summary.txt".format(model_dir, model_str), "w") as f:
             sys.stdout = f
             print(model.summary())
             sys.stdout = og_stdout
@@ -80,7 +81,7 @@ if __name__ == "__main__":
     # parse config
     eps = int(config['training']['epochs'])
     bs = int(config['training']['batch_size'])
-    vs = float(config['training']['validation_size'])
+    vs = float(config['training']['validation_split'])
     start = int(config['training']['start'])
     end = int(config['training']['end'])
     interval = int(config['training']['interval'])
@@ -88,7 +89,7 @@ if __name__ == "__main__":
     # write details to log file (seperate from tensorboard logs)
     log = open("{}/log.txt".format(model_dir), "a+")
     log.write("Model: {}_v{}".format(config['model']['name'],config['model']['ver']))
-    log.write("Epochs: {}".format(epochs))
+    log.write("Epochs: {}".format(eps))
     log.write("Batch Size: {}".format(bs))
     log.write("Validation split: {}".format(vs))
     log.write("Starting patient no: {}".format(start))
@@ -98,15 +99,15 @@ if __name__ == "__main__":
     # train on batches of patients with size __interval__
     # model and weights are saved at each interval
     cur_start = start
-    while cur_start < end
+    while cur_start < end:
         cur_end = min(cur_start+interval, end)
         x,y = generate_data(cur_start, cur_end)
 
         # tensorboard configuration (needs 
         tb_log_dir = "{}/tb_logs/fit/".format(model_dir) + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-        tb_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        tb_callback = tf.keras.callbacks.TensorBoard(log_dir=tb_log_dir, histogram_freq=1)
 
-        model.fit(x,y, epohs=eps, batch_size=bs, validation_size=vs, shuffle=True
+        model.fit(x,y, epochs=eps, batch_size=bs, validation_split=vs, shuffle=True,
                   callbacks=[tb_callback])
         model.save("{}/{}".format(model_dir, model_str))
         model.save_weights("{}/{}_w".format(model_dir, model_str))
