941bfcc07139206d02849b7946c5e2082128db3a
toehlmer <treyoehmler@gmail.com>
2021-02-15 14:17:48 -0800

    add mask generator

--- predict.py
+++ predict.py
@@ -8,10 +8,37 @@ import numpy as np
 import matplotliby.pyplot as plt
 
 
+def save_mask(bg, mask, patient_no, out_path, model_str, slice_no):
+    ones = np.argwhere(mask == 1)
+    twos = np.argwhere(mask == 2)
+    threes = np.argwhere(mask == 3)
+    fours = np.argwhere(mask == 4)
+    bg = color.gray2rgb(bg)
+    bg = rescale_intensity(bg, in_range=(0,1))
+    bg = adjust_gamma(bg, 0.65)
+    bg_copy = bg.copy()
+
+    red = [1, 0.2, 0.2]
+    yellow = [1, 1, 0.25]
+    green = [0.35, 0.75, 0.25]
+    blue = [0, 0.25, 0.9]
+
+    fig = plt.figure()
+    for i in range(ones.shape[0]):
+        bg_copy[ones[i][0]][ones[i][1]] = red
+    for i in range(twos.shape[0]):
+        bg_copy[twos[i][0]][twos[i][1]] = green 
+    for i in range(threes.shape[0]):
+        bg_copy[threes[i][0]][threes[i][1]] = blue 
+    for i in range(fours.shape[0]):
+        bg_copy[fours[i][0]][fours[i][1]] = yellow
+    plt.imshow(bg_copy)
+    plt.savefig('{}/{}_pat{}_slice{}.png'.format(out_path, model_str, patient, slice_no))
 
 
 
-
+
+
 if __name__ == "__main__":
     patient_no = input("Patient no: ")
 
