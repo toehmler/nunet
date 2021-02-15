eb995e66d60fc15e0f8a8610c3a60d20537e43d6
toehlmer <treyoehmler@gmail.com>
2021-02-15 15:06:31 -0800

    added gif creation

--- predict.py
+++ predict.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage.exposure import adjust_gamma, rescale_intensity
 from skimage import color
+import imageio
 
 
 def save_mask(bg, mask, patient_no, out_path, model_str, slice_no):
@@ -16,7 +17,7 @@ def save_mask(bg, mask, patient_no, out_path, model_str, slice_no):
     threes = np.argwhere(mask == 3)
     fours = np.argwhere(mask == 4)
     bg = color.gray2rgb(bg)
-    bg = rescale_intensity(bg, in_range=(0,1))
+#    bg = rescale_intensity(bg, in_range=(0,1))
     bg = adjust_gamma(bg, 0.65)
     bg_copy = bg.copy()
 
@@ -40,24 +41,27 @@ def save_mask(bg, mask, patient_no, out_path, model_str, slice_no):
 
 
 
+def create_gif(out_path, model_str, patient_no):
+    images = glob('{}/{}_pat{}*.png'.format(out_path, model_str, patient_no))
+    img_list = []
+    for fname in images:
+        img_list.append(imageio.imread(fname))
+    imageio.imwrite('{}/{}_pat{}.gif', img_list)
+
 if __name__ == "__main__":
     patient_no = input("Patient no: ")
-
     # parse config file
-
     config = configparser.ConfigParser()
     config.read('config.ini')
     model_str = "{}_v{}".format(config['model']['name'], config['model']['ver'])
     model_dir = "models/{}".format(model_str)
     lr = 1e-4
-
     # load and compile model
     model = load_model("{}/{}".format(model_dir, model_str),
                        custom_objects={"dice_coef": dice_coef,
                                        "dice_coef_loss": dice_coef_loss})
     model.load_weights("{}/{}_w".format(model_dir, model_str))
     model.compile(optimizer=Adam(lr=lr), loss=dice_coef_loss, metrics=[dice_coef])
-
     # load and normalize patient scans
     root = config['general']['root_path']
     out_path = config['general']['image_out_path']
@@ -65,17 +69,17 @@ if __name__ == "__main__":
     path = glob(path_str)[0]
     scans = norm_scans(load_scans(path))
     pbar = tqdm(total = scans.shape[0])
-
+    # predict each slice and generate image from each prediction 
     for slice_no in range(scans.shape[0]):
         test_slice = scans[slice_no:slice_no+1,:,:,:4]
         test_label = scans[slice_no:slice_no+1,:,:,4]
         prediction = model.predict(test_slice, batch_size=32)[0]
         prediction = np.around(prediction)
         prediction = np.argmax(prediction, axis=-1)
-
-        scan = test_slice[0,:,:,0]
+        # [flair, t1, t1c, t2, gt]
+        scan = test_slice[0,:,:,2] # should use t1c scan as background
         save_mask(scan, prediction, patient_no, out_path, model_str, slice_no)
-
+        create_gif(out_path, model_str, patient_no)
         '''
         label = test_label[0]
         im = plt.figure(figsize=(15,10))
